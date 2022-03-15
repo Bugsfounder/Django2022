ec9190682b8b45bb817397113293bcc6b54c0581
Bugs Founder <bugsfounder2021@gmail.com>
2022-03-15 17:04:13 +0530

    Creating HomePage and Getting text using GET Request | Django TextUtils

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,25 +1,35 @@
 # I have created this file - Manisha
-from django.http import  HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
+
 
 # views here
 def index(request):
     return render(request, 'index.html')
 
+
 def removepunc(request):
+    # GET THE TEXT FROM GET REQUEST PARAMS
+    reqText = request.GET.get('text', 'default')
+    print(reqText)
     return HttpResponse("removepunc")
+
 
 def capitalizefirst(request):
     return HttpResponse("capitalizefirst")
 
+
 def newlineremove(request):
     return HttpResponse('newlineremove')
+
 
 def spaceremove(request):
     return HttpResponse('spaceremove')
 
+
 def charactercounte(request):
     return HttpResponse('charactercounte')
+
 
 def titlecase(request):
     return HttpResponse('titlecase')
