98985c60ded11c7a0b1f0f8d0f9498a415367c1b
Bugs Founder <bugsfounder2021@gmail.com>
2022-03-17 07:31:30 +0530

    Adding Media Directory In Django

--- myAwesomeCart/shop/models.py
+++ myAwesomeCart/shop/models.py
@@ -1,8 +1,18 @@
+from distutils.command.upload import upload
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
-    product_description = models.CharField(max_length=300)
+    product_description = models.CharField(max_length=300, default="")
+    price = models.IntegerField(default=0)
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    image = models.ImageField(upload_to="shop/images", default="")
     pub_date = models.DateTimeField()
+    
+    
+    def __str__(self) -> str:
+        return self.product_name
